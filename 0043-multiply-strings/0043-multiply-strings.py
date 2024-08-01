82ee036e64541451e7a8e8124cb562889f6b1fd8
syalal <37207628+syalal@users.noreply.github.com>
2024-08-02 01:25:36 +0530

    Time: 102 ms (23.99%), Space: 16.6 MB (16.59%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,16 +1,20 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        l1 = len(num1)
+        l2 = len(num2)
 
-        num_mapping = {str(i):i for i in range(10)}
+        num1 = num1[::-1]
+        num2 = num2[::-1]
 
-        def string_to_num(str_num):
-            num = 0 
-            for char in str_num:
-                num = num*10 + num_mapping[char]
-            return num
+        res = [0]*(l1+l2)
 
-        num1 = string_to_num(num1)
-        num2 = string_to_num(num2)
+        for i in range(l1):
+            for j in range(l2):
+                mul = int(num1[i])*int(num2[j])
+                res[i+j] += mul
+                res[i+j+1] += res[i+j]//10
+                res[i+j] = res[i+j]%10
 
-        return str(num1*num2)
+        res_str = ''.join(map(str, res[::-1])).lstrip('0')
 
+        return res_str if len(res_str) > 0 else '0'
